fad942ba9035ec1a40f87ccdd87dc14330b5676b
PummelerDev <PummelerDev@users.noreply.github.com>
2022-09-08 22:41:28 +0300

    sandglass and other
    completed the task of drawing an sandglass and other objects

--- src/Sandglass.java
+++ src/Sandglass.java
@@ -57,7 +57,7 @@ public class Sandglass {
 			System.out.println();
 		}
 		System.out.println();
-//		
+// точки в центре	
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 				if (i == 1 && j == 1 || i == 1 && j == 3 || i == 3 && j == 1 || i == 3 && j == 3)
